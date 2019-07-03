fcebe98765b5e0129101602dba85c0662083228a
williammango <williammango2015@gmail.com>
2019-07-03 11:17:17 +0300

    add configs

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+class Config:
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:password@localhost/'
+    QUOTES_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+Config_options ={
+    "development":DevConfig,
+    "production":ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,2 @@
+export SECRET_KEY='mango14'
+python3.6 manage.py server
