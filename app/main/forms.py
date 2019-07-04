2f68c8ec25e042a67b2c7f57d2d5d22958dc8636
williammango <williammango2015@gmail.com>
2019-07-04 14:25:36 +0300

    add update updates

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,3 +7,7 @@ class BlogForm(FlaskForm):
     content = TextAreaField('pitch', validators=[Required()])
                             
     submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
